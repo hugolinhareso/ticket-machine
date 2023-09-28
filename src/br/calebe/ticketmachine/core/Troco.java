20967b9f2fc4a56a92d4bec6c76fbf809b906c0e
akajhon <joaopedrorosa03@gmail.com>
2023-09-28 20:39:13 -0300

    fix: fixed while loops in Troco constructor

--- src/br/calebe/ticketmachine/core/Troco.java
+++ src/br/calebe/ticketmachine/core/Troco.java
@@ -6,42 +6,19 @@ import java.util.Iterator;
  *
  * @author Calebe de Paula Bianchini
  */
+
 class Troco {
 
     protected PapelMoeda[] papeisMoeda;
+    private final int[] VALORES = {100, 50, 20, 10, 5, 2};
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+        for (int i = 0; i < VALORES.length; i++) {
+            int count = valor / VALORES[i];
+            valor = valor % VALORES[i];
+            papeisMoeda[5 - i] = new PapelMoeda(VALORES[i], count);
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
